84fe109a4352fb46ac72c67048ab9ac00a2c49a2
Irfanlam123 <irfanalam010202@gmail.com>
2024-05-17 13:00:28 +0530

    change

--- src/Components/About/Aboutsec.jsx
+++ src/Components/About/Aboutsec.jsx
@@ -3,11 +3,11 @@ import Abot from "../About/abot.jpg";
 
 const Aboutsec = () => {
   return (
-    <section className="flex flex-col md:flex-row mt-24 md:mt-72 space-y-10 md:space-y-0 md:space-x-8 lg:space-x-32 px-4 md:px-12 lg:px-24">
+    <section className="flex flex-col md:flex-row items-center justify-center mt-12 md:mt-24 space-y-10 md:space-y-0 md:space-x-8 lg:space-x-32 px-4 md:px-12 lg:px-24">
       <div className="flex justify-center md:justify-start">
         <img className="h-64 w-64 md:h-80 md:w-80 lg:h-[300px] lg:w-[300px] rounded-3xl" src={Abot} alt="About" />
       </div>
-      <div className="mt-8 md:mt-20 text-center md:text-left">
+      <div className="mt-8 md:mt-0 text-center md:text-left">
         <h2 className="text-2xl md:text-3xl text-blue-500 font-bold">ABOUT ME</h2>
         <h1 className="text-3xl md:text-4xl text-slate-700 font-bold mt-4 md:mt-6 py-2">
           Front-end Developer <br />
@@ -21,7 +21,7 @@ const Aboutsec = () => {
           My main stack currently is React/Next.js in combination <br className="hidden md:block" /> with
           Tailwind CSS.
         </p>
-        <p className="border-2 border-gray-300 mt-8 md:mt-16 rounded-full"></p>
+        <div className="border-2 border-gray-300 mt-8 md:mt-16 rounded-full w-16 mx-auto"></div>
       </div>
     </section>
   );
